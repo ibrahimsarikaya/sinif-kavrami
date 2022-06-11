75aeff95e16576186b65b5b162dad98ea65c1f9e
ibrahimsarikaya <ibo7534@gmail.com>
2022-06-11 17:29:12 +0300

    Sınıf Kavramı

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sinif_kavrami // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# sinif-kavrami
+# Sınıf Kavramı
+-------------------
+## Codding By İbrahim SARIKAYA
+-------------------
+[My Patika Profile](https://app.patika.dev/ibro)
