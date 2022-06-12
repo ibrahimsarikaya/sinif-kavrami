3e664b92c4a813dc3c483d10a593ff7bb7a0eb3d
ibrahimsarikaya <ibo7534@gmail.com>
2022-06-13 00:13:20 +0300

    Codding

--- Program.cs
+++ Program.cs
@@ -22,32 +22,32 @@ namespace sinif_kavrami
             // *Internal > Kendi bulunduğu proje içerisinde erişim sağlanır.
             // *Protected > Sadece tanımlandığı sınıfta ve/veya o sınıftan kalıtım alan diğer miraslarda kullanılabilir.
 
-            Calisan calisan1 = new Calisan();
+            Calisan calisan1 = new Calisan(); // Çalışan sınıfından bir instance oluşturuldu.
             calisan1.Ad="Ayşe";
             calisan1.Soyad="Kara";
             calisan1.No=23415634;
             calisan1.Departman="İnsan Kaynakları";
 
-            calisan1.CalisanBilgileri();
+            calisan1.CalisanBilgileri(); // Üst satırdaki kodlarda atanan değerler bu kodda yazdırıldı.
             Console.WriteLine("****************");
 
-            Calisan calisan2 = new Calisan();
+            Calisan calisan2 = new Calisan(); // Çalışan sınıfından başka bir instance oluşturuldu.
             calisan2.Ad="Deniz";
             calisan2.Soyad="Arda";
             calisan2.No=25646789;
             calisan2.Departman="Satın Alma";
 
-            calisan2.CalisanBilgileri();
+            calisan2.CalisanBilgileri();// Üst satırdaki kodlarda atanan değerler bu kodda yazdırıldı.
         }
     }
-    class Calisan
+    class Calisan // Çalışan isminde bir sınıf oluşturduk.
     {
-        public string Ad;
+        public string Ad; 
         public string Soyad;
         public int No;
         public string Departman;
 
-        public void CalisanBilgileri()
+        public void CalisanBilgileri() // Yazdırma fonksiyonu oluşturuldu.
         {
             Console.WriteLine("Çalışanın Adı:{0}",Ad);
             Console.WriteLine("Çalışanın Soyadı:{0}",Soyad);
